4c4efdb0561faad2315bad6bd49226abfd920544
Olaf <118319942+Ol3f23@users.noreply.github.com>
2022-11-16 01:35:59 +0100

    Add files via upload

---
+++ kalk/script.js
@@ -0,0 +1,9 @@
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperationTextElement = document.querySelector('[data-previous]')
+const currentOperationTextElement = document.querySelector('[data-current]')
+  
+function
