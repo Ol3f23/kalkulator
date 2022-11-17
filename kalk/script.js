f6e29f817ad7d428a7dff263039395e808985921
Olaf <118319942+Ol3f23@users.noreply.github.com>
2022-11-18 00:34:59 +0100

    Delete kalk directory

--- kalk/script.js
+++
@@ -1,9 +0,0 @@
-const numberButtons = document.querySelectorAll('[data-number')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperationTextElement = document.querySelector('[data-previous]')
-const currentOperationTextElement = document.querySelector('[data-current]')
-  
-function
